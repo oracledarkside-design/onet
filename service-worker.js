aaa76608e753ecef61b764cda24a638adb84e352
oracledarkside-design <oracledarkside@gmail.com>
2025-10-04 16:37:42 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "block-game-cache-v8";
+const CACHE_NAME = "block-game-cache-v9";
 const URLS_TO_CACHE = [
   "/onet/",
   "/onet/index.html",
@@ -58,6 +58,7 @@ self.addEventListener("fetch", (event) => {
     );
   }
 });
+
 
 
 
